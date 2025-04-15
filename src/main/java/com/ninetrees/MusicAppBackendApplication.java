9ce481d276eed8972fa2c4f51164b7233773d57c
hr_xin <xinhaoran1@gmail.com>
2025-04-15 11:28:55 +0800

    refactor: move aliyun keys to .env file

--- src/main/java/com/ninetrees/MusicAppBackendApplication.java
+++ src/main/java/com/ninetrees/MusicAppBackendApplication.java
@@ -1,4 +1,5 @@
 package com.ninetrees;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,11 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 public class MusicAppBackendApplication {
 
     public static void main(String[] args) {
+        // 加载 .env 文件
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("ALIYUN_ACCESS_KEY_ID", dotenv.get("ALIYUN_ACCESS_KEY_ID"));
+        System.setProperty("ALIYUN_ACCESS_KEY_SECRET", dotenv.get("ALIYUN_ACCESS_KEY_SECRET"));
+
         SpringApplication.run(MusicAppBackendApplication.class, args);
     }
 
